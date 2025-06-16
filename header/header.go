32ff422305886bc3fa8863d4abfbd7bb93c74d38
rima1881 <amd.gharibipour@outlook.com>
2025-06-16 14:25:54 -0400

    removed bunch of things :O

--- header/header.go
+++ header/header.go
@@ -1,17 +1,12 @@
 package header
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/binary"
-	"io"
-	"net"
-)
+	"errors"
 
-const STATUDBYTEINDEX = 0
-const OPERATIONCODEBYTEINDEX = 1
-const MESSAGELENGTHBYTEINDEXSTART = 2
-const MESSAGEBYTECOUNT = 4
+	"github.com/Pois-Noir/Botzilla-Utils/global_configs"
+)
 
 // pipe & filter style (dont care where its coming from and where it is going)
 // Header is the 5-byte prefix
@@ -21,20 +16,21 @@ type Header struct {
 	Length        uint32 //4 bytes payload length int
 }
 
-func NewEmptyHeader() *Header {
-	return &Header{}
-}
+// func NewEmptyHeader() *Header {
+// 	return &Header{}
+// }
 
-func NewHeader(status uint8, operationCode uint8) *Header {
+func NewHeader(status uint8, operationCode uint8, len uint32) *Header {
 	return &Header{
 		Status:        status,
 		OperationCode: operationCode,
+		Length:        len,
 	}
 }
 
-func (h *Header) SetMessageLength(messageLen uint32) {
-	h.Length = messageLen
-}
+// func (h *Header) SetMessageLength(messageLen uint32) {
+// 	h.Length = messageLen
+// }
 
 func (h *Header) Encode() ([]byte, error) {
 	var buf bytes.Buffer
@@ -58,31 +54,47 @@ func (h *Header) Encode() ([]byte, error) {
 
 }
 
-func DecodeHeader(conn net.Conn) (*Header, error) {
+func Decode(buffer []byte) (*Header, error) {
 
-	var headerBytes [6]byte
-
-	_, err := io.ReadFull(conn, headerBytes[:])
-	if err != nil {
-		// handle error, e.g., log or return
-		return nil, err
+	if len(buffer) != global_configs.HEADER_LENGTH {
+		return nil, errors.New("buffer size is not correct")
 	}
 
-	header := NewHeader(uint8(headerBytes[STATUDBYTEINDEX]), uint8(headerBytes[OPERATIONCODEBYTEINDEX]))
-	header.SetMessageLength(binary.BigEndian.Uint32(headerBytes[MESSAGELENGTHBYTEINDEXSTART : MESSAGELENGTHBYTEINDEXSTART+MESSAGEBYTECOUNT]))
+	header := &Header{}
+
+	header.OperationCode = uint8(buffer[global_configs.STATUS_CODE_INDEX])
+	header.Status = uint8(buffer[global_configs.OPERATION_CODE_INDEX])
+	header.Length = binary.BigEndian.Uint32(buffer[global_configs.MESSAGE_LENGTH_INDEX:global_configs.HEADER_LENGTH])
+
 	return header, nil
+
 }
 
-func DecodeHeaderBuffered(bReader *bufio.Reader) (*Header, error) {
-	var headerBytes [6]byte
+// func DecodeHeader(conn net.Conn) (*Header, error) {
 
-	_, err := io.ReadFull(bReader, headerBytes[:])
-	if err != nil {
-		// handle error, e.g., log or return
-		return nil, err
-	}
+// 	var headerBytes [6]byte
 
-	header := NewHeader(uint8(headerBytes[STATUDBYTEINDEX]), uint8(headerBytes[OPERATIONCODEBYTEINDEX]))
-	header.SetMessageLength(binary.BigEndian.Uint32(headerBytes[MESSAGELENGTHBYTEINDEXSTART : MESSAGELENGTHBYTEINDEXSTART+MESSAGEBYTECOUNT]))
-	return header, nil
-}
+// 	_, err := io.ReadFull(conn, headerBytes[:])
+// 	if err != nil {
+// 		// handle error, e.g., log or return
+// 		return nil, err
+// 	}
+
+// 	header := NewHeader(uint8(headerBytes[STATUDBYTEINDEX]), uint8(headerBytes[OPERATIONCODEBYTEINDEX]))
+// header.SetMessageLength(binary.BigEndian.Uint32(headerBytes[MESSAGELENGTHBYTEINDEXSTART : MESSAGELENGTHBYTEINDEXSTART+MESSAGEBYTECOUNT]))
+// 	return header, nil
+// }
+
+// func DecodeHeaderBuffered(bReader *bufio.Reader) (*Header, error) {
+// 	var headerBytes [6]byte
+
+// 	_, err := io.ReadFull(bReader, headerBytes[:])
+// 	if err != nil {
+// 		// handle error, e.g., log or return
+// 		return nil, err
+// 	}
+
+// 	header := NewHeader(uint8(headerBytes[STATUDBYTEINDEX]), uint8(headerBytes[OPERATIONCODEBYTEINDEX]))
+// 	header.SetMessageLength(binary.BigEndian.Uint32(headerBytes[MESSAGELENGTHBYTEINDEXSTART : MESSAGELENGTHBYTEINDEXSTART+MESSAGEBYTECOUNT]))
+// 	return header, nil
+// }
